0a2f9f71dca42231cbf6cf57bbd118305e05c71c
Yuki Bobier <engineer.yuki.bobier@gmail.com>
2023-01-08 11:05:12 +0000

    test: modify test

--- tests/test_fulltextsearchengine.py
+++ tests/test_fulltextsearchengine.py
@@ -5,17 +5,17 @@ class TestEngine:
     def test_save_and_search(self):
         engine = fulltextsearchengine.createSimplestEngine()
 
-        text1 = "ここは東京ですか、東京ではないですか。"
-        text2 = "ここは東京です。"
-        text3 = "ここは埼玉です。"
-        text4 = "ここは東京の東京による東京のための東京です。"
+        text1 = "走って走っていやいややっぱ歩いて"
+        text2 = "走る雲の影を飛び越えるわ"
+        text3 = "上を向いて歩う涙がこぼれないように"
+        text4 = "動き出すよ君の元へ走れ！走れ！走れ！"
 
         id1 = engine.save(text1)
         id2 = engine.save(text2)
         engine.save(text3)
         id4 = engine.save(text4)
 
-        actual = engine.search("東京")
+        actual = engine.search("走る")
 
         expected = [(id4, text4), (id1, text1), (id2, text2)]
 
